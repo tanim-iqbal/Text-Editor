1eef2c874cd11790fbfbe6dc9d502a9e69da7875
NY4TI <tanim.iqbal13@gmail.com>
2022-01-28 23:58:14 -0500

    heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'JATE'
       }),
      
       // Injects our custom service worker
@@ -36,8 +36,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
+        name: 'JATE',
+        short_name: 'JATE',
         description: 'Never forget your contacts!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
